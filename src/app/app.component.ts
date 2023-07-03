cf01d8b51bd6302f896adb94a7cbbbfc7b527f34
sarahborkan <sarah.behaz1@gmail.com>
2023-07-03 09:05:22 +0200

    modifs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TD1Ex1';
+  title = 'Mon app';
+  nom: string = "Jean";
+  verif: boolean = true;
+  element: number = 1;
+
+  etudiants: any[] = [
+    { "nom": "Toto", "prenom": "claire", "age": 30 },
+    { "nom": "Perle", "prenom": "Prune", "age": 13 },
+    { "nom": "Titi", "prenom": "marie", "age": 30 }
+  ];
+
+  getNomClass(premier: boolean, dernier: boolean, pair: boolean) {
+    var nomClass = "";
+    if (premier || dernier) {
+      nomClass += "gras";
+    }
+
+    if (pair) {
+      nomClass += " rouge";
+    } else {
+      nomClass += " bleu";
+    }
+    return nomClass;
+  }
 }
